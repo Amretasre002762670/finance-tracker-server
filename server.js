68d3e5e70a913f8e5761985b0d2b14db602e6863
amretasrerengarajan <rengarajanthiruven.a@northeastern.edu>
2025-04-07 16:12:35 -0500

    Added health route and other route checks

--- server.js
+++ server.js
@@ -41,9 +41,20 @@ app.use(express.json());
 app.use('/auth', authRoutes);
 app.use('/transactions', transactionsRoutes);
 
+app.get('/health', async (req, res) => {
+    try {
+        await sequelize.authenticate();
+        return res.status(200).json({ message: 'Server is healthy and database connection is successful' });
+    } catch (error) {
+        res.status(500).json({ message: 'Server is healthy but database connection failed' });
+    }
+});
+
 app.use((req, res, next) => {
     res.status(404).json({ message: 'Route not found' });
 });
+
+
 
 sequelize.sync().then(() => {console.log("Database synced")});
 
